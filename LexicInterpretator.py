1959dc963ffdad5cbce723eac4a7315ff938ea6d
LongDude <teplov.vladimir.2004@mail.ru>
2024-12-12 14:32:07 +0300

    statr

--- LexicInterpretator.py
+++ LexicInterpretator.py
@@ -4,9 +4,13 @@ from LexicTokenTypes import *
 class LexicInterpretator(object):
     _root: TreeNode # Узел
     _namespace_table = {} # 
-    
     _inter_stack = []
-
     
     def __init__(self, tree_root: TreeNode):
+        pass
+
+    def parse_node(self, node:TreeNode):
+        pass
+
+    def call_func(self, node:TreeNode):
         pass
